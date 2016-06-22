44a70c16ed1664bdd1dc79d4bf17090faaaae0e0
Joey Reyes <jreyes88@gmail.com>
2016-06-22 18:34:17 -0500

    initial commit

---
+++ liri.js
@@ -0,0 +1,21 @@
+var keyData = require('./keys.js');
+
+console.log("--------------------------");
+console.log("My Tweets:");
+console.log(keyData.my-tweets);
+console.log("--------------------------");
+
+console.log("--------------------------");
+console.log("Spotify This Song:");
+console.log(keyData.spotify-this-song);
+console.log("--------------------------");
+
+console.log("--------------------------");
+console.log("Movie This:");
+console.log(keyData.movie-this);
+console.log("--------------------------");
+
+console.log("--------------------------");
+console.log("Movie This:");
+console.log(keyData.do-what-it-says);
+console.log("--------------------------");
